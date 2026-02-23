a27e725ecc258b3bb60a8a0b8202a8a03deaac5a
Cristian Paul Baroado <baroado.cristian12@gmail.com>
2026-02-23 07:38:42 +0000

    When the turning motor is activated, the tray rotates 180 degrees and pa

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,16 +89,16 @@ export default {
           },
         },
         'egg-turn': {
-          '0%, 100%': { transform: 'rotate(-1.5deg)' },
-          '50%': { transform: 'rotate(1.5deg)' },
-        }
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '25%, 75%': { transform: 'rotate(180deg)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'pulse-slow': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         'pulse-rapid': 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'egg-turn': 'egg-turn 3s ease-in-out infinite',
+        'egg-turn': 'egg-turn 4s ease-in-out infinite',
       },
     },
   },
